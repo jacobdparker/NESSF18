236dbccdd4a2e50a1fea6616277632b2c541322a
jacobdparker <jacobdparker@gmail.com>
2018-02-01 08:03:28 -0700

    wrote personal statement and assembled all piece parts

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # NESSF18
+
+Full proposal including all parts NESSF2018_Jacob_Parker.pdf
---
+++ combine_pdf.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+pdftk ./Personal_Statement/Personal_Statement_NESSF18.pdf ./Proposal/NESSF2018_Proposal.pdf  cv.pdf cv4ck_2p.pdf  nessf18parkerLetter.pdf nessf18parkerAffirmation.pdf  Academic_Transcript.pdf cat output NESSF2018_Jacob_Parker.pdf
